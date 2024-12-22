83a3eeba36c60521ef50191f46f38076d9cba18a
Necas <mounir.hedna@yahoo.com>
2024-12-22 15:52:35 +0000

    Add workflow to update detections

--- development/update_alert.py
+++ development/update_alert.py
@@ -13,47 +13,50 @@ headers = {
     "Authorization": f"ApiKey {api_key}"
 }
 
+changed_files = os.getenv("CHANGED_FILES")
+
 data = ""
 
 for root, dirs, files in os.walk("detections/"):
     for file in files:
-        data = "{\n"
-        if file.endswith(".toml"):
-            full_path = os.path.join(root, file)
-            with open(full_path, "rb") as toml:
-                alert = tomllib.load(toml)
+        if file in changed_files:
+            data = "{\n"
+            if file.endswith(".toml"):
+                full_path = os.path.join(root, file)
+                with open(full_path, "rb") as toml:
+                    alert = tomllib.load(toml)
 
-                if alert["rule"]["type"] == "query":
-                    required_fields = ["author", "description", "name", "risk_score", "severity", "type", "query", "threat", "rule_id"]
-                elif alert["rule"]["type"] == "eql":
-                    required_fields = ["author", "description", "name", "risk_score", "severity", "type", "query", "language", "threat", "rule_id"]
-                elif alert["rule"]["type"] == "threshold":
-                    required_fields = ["author", "description", "name", "risk_score", "severity", "type", "query", "threshold", "threat", "rule_id"]
-                else:
-                    print(f"[!] ERROR: Unsupported rule type [{alert['rule']['type']}] found in <{file}>")
-                    break
+                    if alert["rule"]["type"] == "query":
+                        required_fields = ["author", "description", "name", "risk_score", "severity", "type", "query", "threat", "rule_id"]
+                    elif alert["rule"]["type"] == "eql":
+                        required_fields = ["author", "description", "name", "risk_score", "severity", "type", "query", "language", "threat", "rule_id"]
+                    elif alert["rule"]["type"] == "threshold":
+                        required_fields = ["author", "description", "name", "risk_score", "severity", "type", "query", "threshold", "threat", "rule_id"]
+                    else:
+                        print(f"[!] ERROR: Unsupported rule type [{alert['rule']['type']}] found in <{file}>")
+                        break
 
-                for field in alert["rule"]:
-                    if field in required_fields:
-                        if type(alert["rule"][field]) == list:
-                            data += "  " + "\"" + field + "\": " + str(alert["rule"][field]).replace("'", "\"") + ",\n"
-                        elif type(alert["rule"][field]) == str:
-                            if field == "description":
-                                data += "  " + "\"" + field + "\": " + "\"" + str(alert["rule"][field]).replace("\n", " ").replace("\"", "\\\"").replace("\\", "\\\\") + "\"" + ",\n"
-                            elif field == "query":
-                                data += "  " + "\"" + field + "\": " + "\"" + str(alert["rule"][field]).replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", " ") + "\"" + ",\n"
-                            else:
-                                data += "  " + "\"" + field + "\": " + "\"" + str(alert["rule"][field]).replace("\n", " ").replace("\"", "\\\"") + "\"" + ",\n"
-                        elif type(alert["rule"][field]) == int:
-                            data += "  " + "\"" + field + "\": " + str(alert["rule"][field]) + ",\n"
-                        elif type(alert["rule"][field]) == dict:
-                            data += "  " + "\"" + field + "\": " + str(alert["rule"][field]).replace("'", "\"") + ",\n"
-                data += "  \"enabled\": true\n}"
-            rule_id = alert["rule"]["rule_id"]
+                    for field in alert["rule"]:
+                        if field in required_fields:
+                            if type(alert["rule"][field]) == list:
+                                data += "  " + "\"" + field + "\": " + str(alert["rule"][field]).replace("'", "\"") + ",\n"
+                            elif type(alert["rule"][field]) == str:
+                                if field == "description":
+                                    data += "  " + "\"" + field + "\": " + "\"" + str(alert["rule"][field]).replace("\n", " ").replace("\"", "\\\"").replace("\\", "\\\\") + "\"" + ",\n"
+                                elif field == "query":
+                                    data += "  " + "\"" + field + "\": " + "\"" + str(alert["rule"][field]).replace("\\", "\\\\").replace("\"", "\\\"").replace("\n", " ") + "\"" + ",\n"
+                                else:
+                                    data += "  " + "\"" + field + "\": " + "\"" + str(alert["rule"][field]).replace("\n", " ").replace("\"", "\\\"") + "\"" + ",\n"
+                            elif type(alert["rule"][field]) == int:
+                                data += "  " + "\"" + field + "\": " + str(alert["rule"][field]) + ",\n"
+                            elif type(alert["rule"][field]) == dict:
+                                data += "  " + "\"" + field + "\": " + str(alert["rule"][field]).replace("'", "\"") + ",\n"
+                    data += "  \"enabled\": true\n}"
+                rule_id = alert["rule"]["rule_id"]
 
-            try:
-                elastic_data = requests.put(f"{url}?rule_id={rule_id}", headers=headers, data=data)
-                elastic_data.raise_for_status()
-                print(f"[*] SUCCESS: Successfully updated rule <{file}>")
-            except:
-                print(f"[!] ERROR: {str(elastic_data.content)} for file <{file}>")
+                try:
+                    elastic_data = requests.put(f"{url}?rule_id={rule_id}", headers=headers, data=data)
+                    elastic_data.raise_for_status()
+                    print(f"[*] SUCCESS: Successfully updated rule <{file}>")
+                except:
+                    print(f"[!] ERROR: {str(elastic_data.content)} for file <{file}>")
